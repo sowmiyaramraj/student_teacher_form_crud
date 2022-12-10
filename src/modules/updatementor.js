9a7ed560a1143472c0052e97c6f2f7a3843441bf
sowmiyaramraj <sowmiyaramraj.0505@gmail.com>
2022-12-11 00:45:43 +0530

    -

---
+++ src/modules/updatementor.js
@@ -0,0 +1,138 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import InputLabel from "@mui/material/InputLabel";
+
+function Liststudent() {
+  let formValues = {
+    id: "",
+    name: "",
+    age: "",
+    email: "",
+    gender: "",
+    mobilenumber: "",
+    error: {
+      name: "",
+      age: "",
+      email: "",
+      gender: "",
+      mobilenumber: "",
+    },
+  };
+  const [formData, setFormData] = useState(formValues);
+  const [userData, setUserData] = useState([]);
+
+
+
+//   get axios call
+
+  
+const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Delete
+    const errKeys = Object.keys(formData).filter((key) => {
+      if (formData[key] === "" && key != "error" && key != "id") {
+        return key;
+      }
+    });
+    if (errKeys.length >= 1) {
+      alert("Please fill all values");
+    } else {
+      if (formData.id) {
+        // Update
+        const response = await axios.put(
+          `https://632f42ebb56bd6ac45adc26a.mockapi.io/user/${formData.id}`,
+          {
+            name: formData.name,
+            age: formData.age,
+            email: formData.email,
+            gender: formData.gender,
+            mobilenumber: formData.mobilenumber,
+          }
+        );
+        let users = [...userData];
+        let index = users.findIndex((row) => row.id === response.data.id);
+        users[index] = response.data;
+        setUserData(users);
+      } else {
+        // Create
+        const response = await axios.post(
+          "https://632f42ebb56bd6ac45adc26a.mockapi.io/user",
+          {
+            name: formData.name,
+            age: formData.age,
+            email: formData.email,
+            gender: formData.gender,
+            mobilenumber: formData.mobilenumber,
+          }
+        );
+        setUserData([...userData, response.data]);
+      }
+      setFormData(formValues);
+    }
+  };
+  const onPopulateData = (id) => {
+    const selectedData = userData.filter((row) => row.id === id)[0];
+    setFormData({
+      ...formData,
+      ...selectedData,
+    });
+  };
+
+
+ 
+  return (
+    <div style={{ padding: "20px" }}>
+      
+      <h3> Student Data </h3>
+      <TableContainer component={Paper}>
+        <Table sx={{ width: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Id</TableCell>
+              <TableCell align="right">Name</TableCell>
+              <TableCell align="right">Age</TableCell>
+              <TableCell align="right">Email</TableCell>
+              <TableCell align="right">Gender</TableCell>
+              <TableCell align="right">mobilenumber</TableCell>             
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {userData.map((row) => (
+              <TableRow key={row.id}>
+                <TableCell component="th" scope="row">
+                  {row.id}
+                </TableCell>
+                <TableCell align="right">{row.name}</TableCell>
+                <TableCell align="right">{row.age}</TableCell>
+                <TableCell align="right">{row.email}</TableCell>
+                <TableCell align="right">{row.gender}</TableCell>
+                <TableCell align="right">{row.mobilenumber}</TableCell>
+              
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+}
+
+export default Liststudent;
