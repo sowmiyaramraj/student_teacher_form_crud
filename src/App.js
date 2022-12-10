1cef76c12b770997476ffbcc4bde7efabcfbd381
sowmiyaramraj <sowmiyaramraj.0505@gmail.com>
2022-12-11 00:28:26 +0530

    -

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Listmentor from "./modules/listmentor";
+import Liststudent from "./modules/liststudent";
+import Signupstudent from "./modules/signupstudent";
+import Signupmentor from "./modules/signupmentor";
+import Updatestudent from "./modules/updatestudent";
+import Updatementor from "./modules/updatementor";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Stud/>;
 }
 
 export default App;
---
+++ src/modules/listmentor.js
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import InputLabel from "@mui/material/InputLabel";
+
+function Listmentor() {
+  let formValues = {
+    id: "",
+    name: "",
+    age: "",
+    email: "",
+    gender: "",
+    mobilenumber: "",
+    error: {
+      name: "",
+      age: "",
+      email: "",
+      gender: "",
+      mobilenumber: "",
+    },
+  };
+  const [formData, setFormData] = useState(formValues);
+  const [userData, setUserData] = useState([]);
+
+
+
+//   get axios call
+
+  useEffect(() => {
+    async function listStudent() {
+      const response = await axios.get(
+        "https://632f42ebb56bd6ac45adc26a.mockapi.io/user"
+      );
+      setUserData(response.data);
+    }
+    listStudent();
+  }, []);
+
+ 
+  return (
+    <div style={{ padding: "20px" }}>
+      
+      <h3> Student Data </h3>
+      <TableContainer component={Paper}>
+        <Table sx={{ width: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Id</TableCell>
+              <TableCell align="right">Name</TableCell>
+              <TableCell align="right">Age</TableCell>
+              <TableCell align="right">Email</TableCell>
+              <TableCell align="right">Gender</TableCell>
+              <TableCell align="right">mobilenumber</TableCell>             
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {userData.map((row) => (
+              <TableRow key={row.id}>
+                <TableCell component="th" scope="row">
+                  {row.id}
+                </TableCell>
+                <TableCell align="right">{row.name}</TableCell>
+                <TableCell align="right">{row.age}</TableCell>
+                <TableCell align="right">{row.email}</TableCell>
+                <TableCell align="right">{row.gender}</TableCell>
+                <TableCell align="right">{row.mobilenumber}</TableCell>
+              
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+}
+
+export default Listmentor;
