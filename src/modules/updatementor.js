63ff700365c8a36066d27cdfb79aee04161b48bc
sowmiyaramraj <sowmiyaramraj.0505@gmail.com>
2022-12-11 13:17:14 +0530

    -

--- src/modules/updatementor.js
+++ src/modules/updatementor.js
@@ -21,6 +21,8 @@ import Select from "@mui/material/Select";
 import InputLabel from "@mui/material/InputLabel";
 import { useNavigate,Link,useParams } from 'react-router-dom';
 function Liststudent() {
+  const navigate=useNavigate();
+  const params=useParams();
   let formValues = {
     id: "",
     name: "",
@@ -39,14 +41,53 @@ function Liststudent() {
   const [formData, setFormData] = useState(formValues);
   const [userData, setUserData] = useState([]);
 
+  useEffect(() => {
+    
+    try{
+  async function getData(id){
+      const response = await axios.get(`https://632f42ebb56bd6ac45adc26a.mockapi.io/user/${id}`);
+      const response1 = response.data;
+      setFormData({...formData,
+              id:response1.id,
+              name:response1.name,
+              age:response1.age,
+              email:response1.email,
+              gender:response1.gender,
+              mobilenumber:response1.mobilenumber,                 
+      }); 
+      }     
+      getData(params.userId);//call user data when loading the file
+  
+    }catch(error){
+  
+    }
+    
+ 
+  },[]);
 
+  const onPopulateData = (id) => {
+    const selectedData = userData.filter((row) => row.id === id)[0];
+    setFormData({
+      ...formData,
+      ...selectedData,
+    });
+  };
+
+  const handleChange = (e) => {
+    let error = { ...formData.error };
+    if (e.target.value === "") {
+      error[e.target.name] = `${e.target.name} is Required`;
+    } else {
+      error[e.target.name] = "";
+    }
+    setFormData({ ...formData, [e.target.name]: e.target.value, error });
+  };
 
 //   get axios call
-
   
 const handleSubmit = async (e) => {
     e.preventDefault();
-    // Delete
+   
     const errKeys = Object.keys(formData).filter((key) => {
       if (formData[key] === "" && key != "error" && key != "id") {
         return key;
@@ -71,35 +112,89 @@ const handleSubmit = async (e) => {
         let index = users.findIndex((row) => row.id === response.data.id);
         users[index] = response.data;
         setUserData(users);
-      } else {
-        // Create
-        const response = await axios.post(
-          "https://632f42ebb56bd6ac45adc26a.mockapi.io/user",
-          {
-            name: formData.name,
-            age: formData.age,
-            email: formData.email,
-            gender: formData.gender,
-            mobilenumber: formData.mobilenumber,
-          }
-        );
-        setUserData([...userData, response.data]);
-      }
+        alert('updated successfully');
+      } 
       setFormData(formValues);
     }
   };
-  const onPopulateData = (id) => {
-    const selectedData = userData.filter((row) => row.id === id)[0];
-    setFormData({
-      ...formData,
-      ...selectedData,
-    });
-  };
 
-
- 
+   
   return (
     <div style={{ padding: "20px" }}>
+      <h3> User Form </h3>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "30ch" },
+        }}
+        autoComplete="off"
+        onSubmit={(e) => handleSubmit(e)}
+      >
+        <TextField
+          id="name"
+          label="Name"
+          variant="standard"
+          value={formData.name}
+          name="name"
+          onChange={(e) => handleChange(e)}
+        />
+        <br />
+        <span style={{ color: "red" }}>{formData.error.name}</span>
+        <br />
+        <TextField
+          id="age"
+          label="Age"
+          variant="standard"
+          type="number"
+          name="age"
+          value={formData.age}
+          onChange={(e) => handleChange(e)}
+        />
+        <br />
+        <span style={{ color: "red" }}>{formData.error.age}</span>
+        <br />
+        <TextField
+          id="email"
+          type="email"
+          label="Email"
+          variant="standard"
+          name="email"
+          value={formData.email}
+          onChange={(e) => handleChange(e)}
+        />
+        <br />
+        <span style={{ color: "red" }}>{formData.error.email}</span>
+        <br />
+        <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+        <RadioGroup
+          aria-labelledby="demo-radio-buttons-group-label"
+          name="gender"
+          value={formData.gender}
+          onChange={(e) => handleChange(e)}
+        >
+          <FormControlLabel value="Female" control={<Radio />} label="Female" />
+          <FormControlLabel value="Male" control={<Radio />} label="Male" />
+          <FormControlLabel value="Other" control={<Radio />} label="Other" />
+        </RadioGroup>
+        <br />
+        <span style={{ color: "red" }}>{formData.error.gender}</span>
+        
+        <TextField
+          id="mobilenumber"
+          type="number"
+          label="mobilenumber"
+          variant="standard"
+          name="mobilenumber"
+          value={formData.mobilenumber}
+          onChange={(e) => handleChange(e)}
+        />
+        <br />
+        <span style={{ color: "red" }}>{formData.error.mobilenumber}</span>
+        <br />
+        <Button variant="contained" type="submit">
+          Submit
+        </Button>
+      </Box>
       
       <h3> Student Data </h3>
       <TableContainer component={Paper}>
